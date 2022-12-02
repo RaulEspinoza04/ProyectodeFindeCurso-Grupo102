7c94c0192bdea3ed32f6dfc280b110b25fa1c3e4
Raúl Espinoza <71485921@continental.edu.pe>
2022-12-02 15:17:45 -0500

    Se modifico la H001

--- src/logica/menu.py
+++ src/logica/menu.py
@@ -1,1 +1,14 @@
 #H001
+
+from src.modelo.asignatura import Asignatura
+from src.modelo.docente import Docente
+from src.modelo.estudiante import Estudiante
+from src.modelo.declarative_base import Session, engine, Base
+
+if __name__ == '__main__':
+    # Crea la BD
+    Base.metadata.create_all(engine)
+    # Abre la session
+    session = Session()
+    session.commit()
+    session.close()
